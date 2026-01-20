96ae94fe2eeeb654f76459b6258e66141045abb3
Håkon Husby <hakohusb@playgroundsquad.com>
2026-01-20 17:06:35 +0100

    camera movement progress (i hope)

--- Assets/Task3Content/Scripts/Player.cs
+++ Assets/Task3Content/Scripts/Player.cs
@@ -7,6 +7,11 @@ namespace GameTask3
 {
     public class Player : MonoBehaviour
     {
+        [Header("Player Settings")]
+        [SerializeField] private float sensitivity;
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float jumpHeight;
+        [Header("Dependencies")]
         [SerializeField] private CinemachineFollow cinemachineFollow;
         [SerializeField] private GameManager gameManager;
         [SerializeField] private Rigidbody rb;
@@ -14,7 +19,6 @@ namespace GameTask3
         
         private CharacterController controller;
         
-
         Vector3 velocity;
 
 
@@ -40,10 +44,35 @@ namespace GameTask3
                 Debug.Log("Successfully detected spacebar input");
             }
 
-            if (input.Look.triggered)
-            {
-                Debug.Log("Successfully detected mouse input");
-            }
+
+
+
+
+            { // CAMERA MOVEMENT
+                Vector2 lookDelta = input.Look.ReadValue<Vector2>();
+
+                float mouseX = lookDelta.x * sensitivity;
+                float mouseY = lookDelta.y * sensitivity;
+
+                if (Cursor.lockState == CursorLockMode.Locked)
+                {
+                    transform.Rotate(new Vector3(0.0f, mouseX, 0.0f));
+                    /*
+                    cameraPitch -= mouse_y * 0.035f;
+                    cameraPitch = Mathf.Clamp(
+                        cameraPitch,
+                        -Mathf.PI / 2.0f + 0.1f,
+                        Mathf.PI / 2.0f - 0.1f
+                    );
+
+                    cinemachineFollow.FollowOffset = new Vector3(
+                        0.0f,
+                        cameraDistance * Mathf.Sin(cameraPitch),
+                        -cameraDistance * Mathf.Cos(cameraPitch)
+                    );*/
+                }
+
+            } // CAMERA MOVEMENT
         }
     }
 }
